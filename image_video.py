61ed8366e5961919684169969318913724c7b103
None <59431361+xavier-zw@users.noreply.github.com>
2021-11-23 12:37:10 +0800

    Update image_video.py

--- image_video.py
+++ image_video.py
@@ -1,11 +1,11 @@
 import cv2
 import os
 
-img_root = "/home/xavier/Pycharm/tDense_pose/show"
+img_root = "./show"
 fps = 30
 size = (515, 330)
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-videoWriter = cv2.VideoWriter('/home/xavier/Pycharm/tDense_pose/show.avi', fourcc, fps, size)
+videoWriter = cv2.VideoWriter('show.avi', fourcc, fps, size)
 for x in sorted(os.listdir(img_root), key=lambda x: [int(x.split("_")[0]), x.split("_")[1]]):
     frame = cv2.imread(os.path.join(img_root, x))
     videoWriter.write(frame)
